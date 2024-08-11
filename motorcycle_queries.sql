e0d84e7fb2aa52245907e5258e8df2f58fe3b5fe
Marco Prazeres <105886246+mpprazeres@users.noreply.github.com>
2024-08-11 22:42:56 +0100

    Add files via upload

--- motorcycle_queries.sql
+++ motorcycle_queries.sql
@@ -23,13 +23,12 @@ greenhouse_gases float
 
 SELECT * FROM motorcycle_db
 
--- Number of models per year by brand
+
 SELECT make, year_of_launch, COUNT(DISTINCT model) number_models 
 FROM motorcycle_db
 GROUP BY make, year_of_launch
 ORDER BY make ASC;
 
--- Lessen emissions generated
 SELECT make, model, MIN(greenhouse_gases) emission 
 FROM motorcycle_db
 GROUP BY make, model
@@ -37,21 +36,18 @@ HAVING MAX(greenhouse_gases) > 0
 ORDER BY emission
 LIMIT 5;
 
--- Average consumption by brand by year
 SELECT make, year_of_launch, AVG(fuel_consumption)::Numeric(10,2) average_consumption 
 FROM motorcycle_db
 WHERE fuel_consumption IS NOT NULL
 GROUP BY make, year_of_launch
 ORDER BY year_of_launch;
 
--- Lowest consumption by model
 SELECT make, model, year_of_launch, fuel_consumption 
 FROM motorcycle_db
 WHERE fuel_consumption IS NOT NULL
 ORDER BY fuel_consumption asc
 LIMIT 5;
 
--- Fastest motorcycle by model
 WITH fastest_bikes AS(
 	SELECT make, model, top_speed_km, ROW_NUMBER() OVER (PARTITION BY make ORDER BY top_speed_km DESC NULLS LAST) top_speed
 	FROM motorcycle_db
@@ -60,7 +56,6 @@ SELECT make, model, top_speed_km
 FROM fastest_bikes
 WHERE top_speed = 1;
 
--- Bike with most power and torque by brand
 WITH most_powerful AS(
 	SELECT make, model, Year_of_launch, power_ps, torque_rpm, top_speed_km, ROW_NUMBER() OVER (PARTITION BY make ORDER BY power_ps DESC NULLS LAST , torque_rpm DESC NULLS LAST) bike_power
 	FROM motorcycle_db
@@ -69,13 +64,11 @@ SELECT make, model, Year_of_launch, power_ps, torque_rpm, top_speed_km
 FROM most_powerful
 WHERE bike_power = 1;
 
--- Number of models by brand
 SELECT make, COUNT(DISTINCT model) number_models 
 FROM motorcycle_db
 GROUP BY make
 ORDER BY number_models DESC;
 
--- Evolution of power by brand
 SELECT make, year_of_launch, AVG(power_ps)::Numeric(10,2) average_power
 FROM motorcycle_db
 WHERE power_ps IS NOT NULL
